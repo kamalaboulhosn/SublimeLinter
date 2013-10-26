c20269a8e1415cb2c27ca88b739573b1838678e1
Kamal Aboul-Hosn <kamal@sooloos.com>
2013-10-26 11:15:02 -0400

    Fixed C# linter for Sublime Text 3

--- sublimelinter/modules/csharp.py
+++ sublimelinter/modules/csharp.py
@@ -1,6 +1,6 @@
 import re
 
-from base_linter import BaseLinter, INPUT_METHOD_FILE
+from .base_linter import BaseLinter, INPUT_METHOD_FILE
 
 CONFIG = {
     'language': 'csharp',
@@ -15,7 +15,6 @@ class Linter(BaseLinter):
     def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
         for line in errors.splitlines():
             match = re.match('^.+\((?P<line>\d+),\d+\): error .+: (?P<error>.+)', line)
-            print line
             if match:
                 error, line = match.group('error'), match.group('line')
                 self.add_message(int(line), lines, error, errorMessages)
